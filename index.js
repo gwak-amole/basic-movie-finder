fdcfa9907c404d10d63de4453d5245affdf00f90
gwak-amole <gwaksophie@gmail.com>
2026-07-10 11:20:17 -0700

    Add searched movie title, year, and image

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const search_box = document.getElementById("search");
 const search_button = document.getElementById("search-button");
-const renderMovies = (jsonfile) => {
-    const readable = JSON.parse(jsonfile);
+const div_movies = document.getElementById("movie-show");
+
+const renderMovie = (object) => {
+    div_movies.innerHTML = "";
     const movie_div = document.createElement("div");
     const title = document.createElement("h2");
-    title.textContent = readable.Title;
+    title.textContent = object.Title;
     const year = document.createElement("p");
-    year.textContent = readable.Year;
+    year.textContent = object.Year;
     const img = document.createElement("img");
-    img.src = readable.Poster;
+    img.src = object.Poster;
+    img.classList.add("movie-img")
     movie_div.appendChild(title);
     movie_div.appendChild(year);
     movie_div.appendChild(img);
     movie_div.classList.add('movie-div');
+    div_movies.appendChild(movie_div);
 }
 
 
 const getMovieData = async () => {
     const search_content = search_box.value;
-    const url = `http://www.omdbapi.com/?apikey=f39e4e1b&s=${search_content}`;
+    const url = `http://www.omdbapi.com/?apikey=f39e4e1b&t=${search_content}`;
     try {
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error(`Response: ${response.status}`);
         }
         const result = await response.json();
-        renderMovies(result);
+        console.log(result);
+        renderMovie(result);
 
     } catch (error) {
         console.error(error.message);
